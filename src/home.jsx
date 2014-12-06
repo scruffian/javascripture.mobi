a8fcc9cc78e4ff5a0e29003b2c8a1ddbb86ab70b
Ben Dwyer <ben@scruffian.com>
2014-12-05 22:25:50 -1000

    reference browsing

--- src/home.jsx
+++ src/home.jsx
@@ -36,20 +36,19 @@ var BookControl = React.createClass( {
 			chapter: 1
 		};
 	},
+	goToReference: function() {
+		page( '/' + this.props.name + '/' + this.state.chapter );
+	},
 	handleMouseMove: function( event ) {
 		this.setChapter( event.clientX );
 	},
 	handleTouchMove: function( event ) {
-		this.setChapter( event.clientX );
+		this.setChapter( event.touches[0].clientX );
 	},
 	setChapter: function( clientX ) {
-		var width = this.getDOMNode().offsetWidth;
-		var spacing =  width / this.props.chapters;
-		var chapter = Math.ceil( clientX / spacing );
-
-	//if ( ! event.clientX ) {
-	//	var position = event.originalEvent.touches[0].clientX - $(this).offset().left;
-	//}
+		var width = this.getDOMNode().offsetWidth,
+			spacing =  width / this.props.chapters,
+			chapter = Math.ceil( clientX / spacing );
 
 		this.setState( {
 			'chapter': chapter
@@ -57,12 +56,14 @@ var BookControl = React.createClass( {
 	},
 	render: function() {
 		return (
-			<div onMouseMove={ this.handleMouseMove } onTouchMove={ this.handleTouchMove }>{ this.props.name } { this.state.chapter }</div>
+			<div onClick={ this.goToReference } onTouchEnd={ this.goToReference } onMouseMove={ this.handleMouseMove } onTouchMove={ this.handleTouchMove }>
+				{ this.props.name } { this.state.chapter }
+			</div>
 		);
 	}
 } );
 
-var BookList = React.createClass( {
+var ReferenceSelector = React.createClass( {
 	handleChange: function( event ) {
 		this.setState( {
 			'reference': event.target.value
@@ -83,7 +84,32 @@ var BookList = React.createClass( {
 			);
 		} );
 		return (
-			<div>{ books }</div>
+			<div className="reference-selector">{ books }</div>
+		);
+	}
+} );
+
+var TrayButton = React.createClass( {
+	openTray: function() {
+		document.getElementById( this.props.id ).style.display = 'block';
+	},
+	render: function() {
+		return (
+			<button onClick={ this.openTray }>{ this.props.text }</button>
+		);
+	}
+} );
+
+var Tray = React.createClass( {
+	render: function() {
+		return (
+			<div className="tray">
+				<TrayButton text="Go to" id="goto" />
+				<TrayButton text="Details" id="details" />
+				<TrayButton text="Search" id="search" />
+				<TrayButton text="Bookmarks" id="bookmarks" />
+				<TrayButton text="Settings" id="settings" />
+			</div>
 		);
 	}
 } );
@@ -94,7 +120,8 @@ var Layout = React.createClass( {
 			<div>
 				<Home />
 				<div id="reference"></div>
-				<BookList />
+				<ReferenceSelector />
+				<Tray />
 			</div>
 		);
 	}
