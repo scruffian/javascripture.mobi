ea766f3004e01fdfe45fd518db866ef63db6c7f6
Ben Dwyer <ben@scruffian.com>
2014-12-29 23:55:06 +0000

    add assets

---
+++ src/wordDetails.jsx
@@ -0,0 +1,42 @@
+// External
+var React = require( 'react/addons' );
+
+// Internal
+var strongsDictionary = require( '../data/strongs-dictionary.js' ),
+	wordTracking = require( './wordTracking.js' )();
+
+var WordDetails = React.createClass( {
+	getInitialState: function() {
+		return {
+			words: []
+		};
+	},
+
+	componentWillMount: function() {
+		var self = this;
+		wordTracking.on( 'change', function() {
+			self.setState( {
+				words: this.trackedWords
+			} );
+		} );
+	},
+
+	render: function() {
+		var words = this.state.words.map( function( lemma ) {
+			return (
+				<div key={ lemma }>
+					<div>{ lemma } | { strongsDictionary[ lemma ].lemma } | { strongsDictionary[ lemma ].xlit } | { strongsDictionary[ lemma ].pron }</div>
+					<div>Derivation: { strongsDictionary[ lemma ].derivation }</div>
+					<div>Strongs Definition: { strongsDictionary[ lemma ].strongs_def }</div>
+					<div>KJV Usage: { strongsDictionary[ lemma ].kjv_def }</div>
+				</div>
+			);
+		} );
+
+		return (
+			<div className="word-details">{ words }</div>
+		);
+	}
+} );
+
+module.exports = WordDetails;
